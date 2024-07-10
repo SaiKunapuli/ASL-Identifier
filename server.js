ddb7b599862e00c576d83a2cec5f37dd1b589acd
Samik Shah <samikmshah07@gmail.com>
2024-07-10 11:25:35 -0400

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
