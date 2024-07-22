6310861ed73cd7a2e270c557b3da02442606c63a
FaizD18 <faizpro2018@gmail.com>
2024-07-22 13:31:33 -0400

    fixed the text to image page

--- public/script.js
+++ public/script.js
@@ -1,14 +1,14 @@
 // Mapping of letters to ASL image URLs
 const aslImages = {
-    "a": "ASL Photos\A.jpg",
-    "b": "ASL Photos\B.jpg",
-    "c": "ASL Photos\C.png",
+    "a": "public\\ASL_Photos\\A.jpg",
+    "b": "ASL_Photos\B.jpg",
+    "c": "ASL_Photos\C.png",
     // Add more mappings as needed
 };
 
 // Function to show ASL image based on input letter
-function showASLImage() {
-    const letter = document.getElementById('letterInput').value.trim().toLowerCase();
+function convertToASL() {
+    const letter = document.getElementById('wordInput').value.trim().toLowerCase();
     const aslDisplay = document.getElementById('aslDisplay');
 
     if (aslImages.hasOwnProperty(letter)) {
